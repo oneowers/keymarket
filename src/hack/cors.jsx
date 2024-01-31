e7708cd1282b0fd377137a4a72e9e511d39b07ac
Muhammad Kamolov <muhammadkamolov@MacBook-Air-Muhammad.local>
2024-01-31 22:20:50 +0500

    ByFinch

--- src/hack/cors.jsx
+++ src/hack/cors.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const App = () => {
 
-    const urls = Array.from({ length: 5 }, (_, i) => `http://localhost:3000/hack/olx/${i + 1}`);
+    const urls = Array.from({ length: 5 }, (_, i) => `http://keymarket.uz/hack/olx/${i + 1}`);
 
     // Function to open each URL in a new tab
     function openUrlsInNewTabs() {
